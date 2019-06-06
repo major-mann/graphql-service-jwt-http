828e453fed19bac31b3ebc5eddf6511ed68fe4d9
Calvin Mann <calvinmann@gmail.com>
2019-06-06 23:02:43 +0200

    Changed to backticks (where encounrered) Removed old unused function Increased log level on token failure to assist with debugging

--- src/context.js
+++ src/context.js
@@ -1,14 +1,14 @@
 module.exports = createContextCreator;
 
-const uuid = require('uuid');
+const uuid = require(`uuid`);
 
 function createContextCreator({ verifyRequestToken,
                                 generateToken,
                                 laxTokenHeader,
-                                queryTokenName = 'token',
-                                bodyTokenName = 'token',
-                                authorizationHeaderName = 'authorization',
-                                tokenTypeName = 'bearer',
+                                queryTokenName = `token`,
+                                bodyTokenName = `token`,
+                                authorizationHeaderName = `authorization`,
+                                tokenTypeName = `bearer`,
                                 createLogger = defaultCreateLogger,
                                 createStat = defaultCreateStat,
                                 isInternalUser,
@@ -56,26 +56,27 @@ function createContextCreator({ verifyRequestToken,
                 return claims;
             } catch (ex) {
                 switch (ex.name) {
-                    case 'JsonWebTokenError':
+                    case `JsonWebTokenError`:
                         throw new Error(`Token error: ${ex.message}`);
-                    case 'TokenExpiredError':
+                    case `TokenExpiredError`:
                         throw new Error(`Token expired at ${ex.expiredAt.toISOString()}`);
-                    default:
-                        const id = uuid.v4().replace(/-/g, '');
-                        createLogger(req).debug(`Invalid token "${token}" received in ${source}. ${id} ` +
+                    default: {
+                        const id = uuid.v4().replace(/-/g, ``);
+                        createLogger(req).log(`Invalid token "${token}" received in ${source}. ${id} ` +
                             `${ex.name} ${ex.stack}`);
                         throw new Error(`Internal server error. Please send "${id}" to support to assist with ` +
                             `identifying error`);
+                    }
                 }
             }
         }
     };
 
     function extractBearer(authorization) {
-        if (typeof authorization !== 'string') {
+        if (typeof authorization !== `string`) {
             return undefined;
         }
-        const [type, token] = authorization.split(' ');
+        const [type, token] = authorization.split(` `);
         if (laxTokenHeader) {
             return token || type;
         } else if (type.toLowerCase() === tokenTypeName) {
@@ -84,13 +85,6 @@ function createContextCreator({ verifyRequestToken,
             return undefined;
         }
     }
-}
-
-function determineIssuer(request) {
-    // TODO: Check this is correct and works as expected
-    // TODO: What about port?
-    const issuer = `${request.protocol}://${request.hostname}`;
-    return issuer;
 }
 
 function defaultCreateLogger() {
